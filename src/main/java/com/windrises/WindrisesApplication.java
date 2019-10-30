25b3718d5eed1928a0ab5f6d591ba845c55ee230
jerax <q14341319@gamil.com>
2019-10-30 09:55:58 +0800

    init

---
+++ src/main/java/com/windrises/WindrisesApplication.java
@@ -0,0 +1,13 @@
+package com.windrises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindrisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindrisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/windrises/WindrisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windrises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindrisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
